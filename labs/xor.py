52068c0f7a18a9fca579aa52d59d1de365e4d2b1
Alejandro <alejandro.lorenzo@atventurepartners.tech>
2022-09-23 16:42:34 -0700

    xor example

---
+++ labs/xor.py
@@ -0,0 +1,23 @@
+import random
+
+def xor(x, s):
+    print(f"{bin(x)} xor {bin(s)} = {bin(x ^ s)}")
+
+
+
+def generate_key_stream(n):
+    return bytes([random.randrange(0, 255) for _ in range(n)])
+
+def xor_bytes(key_stream, message):
+    print(len(key_stream))
+    print(len(message))
+    length = min(len(key_stream), len(message))
+    return bytes([key_stream[i] ^ message[i] for i in range(length)])
+
+
+message = "HEY YOU"
+message = message.encode()
+key_stream = generate_key_stream(len(message))
+cipher = xor_bytes(key_stream, message)
+print(cipher)
+print(xor_bytes(key_stream, cipher))
