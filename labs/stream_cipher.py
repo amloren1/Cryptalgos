75ebf9f12bb0efa37ba6b3d7ebd56d028a5247f0
Alejandro <alexlorenzo39@gmail.com>
2022-09-26 03:42:06 -0700

    stream cipher attack

--- labs/stream_cipher.py
+++ labs/stream_cipher.py
@@ -28,11 +28,54 @@ def transmit(cipher, likely):
         b.append(c)
     return bytes(b)
     
-key = KeyStream(11)
-message = "HEY YOU".encode()
-cipher = encrypt(key, message)
-print(cipher)
+def example_1():
+    key = KeyStream(11)
+    message = "HEY YOU".encode()
+    cipher = encrypt(key, message)
+    print(cipher)
 
-key.next = 11
-# decrypt
-print(encrypt(key, cipher))
+    key.next = 11
+    # decrypt
+    print(encrypt(key, cipher))
+
+def modification(cipher):
+    """ modify cipher
+    Proof:
+
+    key ^ message = cipher
+    mod = message ^ new_message
+    cipher' = cipher ^ mod
+    message' = cipher' ^ key = cipher ^ mod ^ key 
+        = cipher ^ message ^ new_message ^ key 
+        = message ^ key ^ message ^ new_message ^ key 
+        = new_message
+    """
+    mod = [0] * len(cipher)
+
+    mod[10] = ord(" ") ^ ord("1")
+    mod[11] = ord(" ") ^ ord("0")
+    mod[12] = ord("1") ^ ord("0")
+    return bytes(mod[i] ^ cipher[i] for i in range(len(cipher)))
+
+
+def example_2():
+    """
+    Message authenticity issues with stream ciphers
+
+    Bob wants to intercept a message from Alice to Bank
+    and modify amount of money sent to Bob
+    """
+    key = KeyStream(10)
+
+    message = "Send Bob:   10$".encode()
+    cipher = encrypt(key, message)
+    print(cipher)
+    print(message)
+
+    # modify
+    mod = modification(cipher)
+    key = KeyStream(10)
+    # decrypt
+    print(encrypt(key, mod))
+
+example_2()
